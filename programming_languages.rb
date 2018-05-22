d304c5ecc070c43684ab47e8506174e94e416b63
ioreka <lucymhodges@gmail.com>
2018-05-22 18:27:48 +0100

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,41 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |topkey, language_data|
+    language_data.each do |language, data|
+      if !new_hash[language]
+        new_hash[language] = {:type => data[:type], :style => [topkey]}
+      else
+        new_hash[language][:style] << topkey
+      end
+    end
+  end
+
+    new_hash
 end
+
+
+
+# example:
+# languages = {
+#   :oo => {
+#     :ruby => {
+#       :type => "interpreted"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     },
+# #
+#
+# to this:
+# {
+#   :ruby => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :javascript => {
+#     :type => "interpreted",
+#     :style => [:oo, :functional]
+#   },
